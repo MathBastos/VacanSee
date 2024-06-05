8589dc44c5ddbf45f3469a72b0ae86a1beb3419e
Matheus Bastos <matheus.hebastos@gmail.com>
2024-06-05 16:57:53 -0300

    Fix editarQuarto

--- VacanSee/js/editaQuarto.js
+++ VacanSee/js/editaQuarto.js
@@ -5,16 +5,16 @@ function editaQuarto(){
         data: "",
         url: "../php/buscaQuarto.php",
         success: function (resultado) {
-            document.getElementById("andar").value = resultado.modelo;
-            document.getElementById("numero").value = resultado.marca;
-            document.getElementById("tipo_cama").value = resultado.ano;
-            document.getElementById("qtd_cama").value = resultado.cambio;
-            document.getElementById("qtd_banheiro").value = resultado.direcao;
-            document.getElementById("banheira").value = resultado.categoria;
-            document.getElementById("ar_condicionado").value = resultado.chassi;
-            document.getElementById("servico_quarto").value = resultado.placa;
-            document.getElementById("cafe_manha").value = resultado.cor;
-            document.getElementById("valor_dia").value = resultado.motor;
+            document.getElementById("andar").value = resultado.andar;
+            document.getElementById("numero").value = resultado.numero;
+            document.getElementById("tipo_cama").value = resultado.tipo_cama;
+            document.getElementById("qtd_cama").value = resultado.qtd_cama;
+            document.getElementById("qtd_banheiro").value = resultado.qtd_banheiro;
+            document.getElementById("banheira").value = resultado.banheira;
+            document.getElementById("ar_condicionado").value = resultado.ar_condicionado;
+            document.getElementById("servico_quarto").value = resultado.servico_quarto;
+            document.getElementById("cafe_manha").value = resultado.cafe_manha;
+            document.getElementById("valor_dia").value = resultado.valor_dia;
         },
         error: function (){
             
